7fc3e8dd682ee1e6cfc191c7ea667ba1208f1a8d
Nabster2010 <58792560+Nabster2010@users.noreply.github.com>
2022-11-17 03:11:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="min-h-screen overflow-hidden  text-white flex justify-center items-center  bg-[url('assets/images/bg02.jpg')] bg-cover bg-center bg-no-repeat  ">
+    <div className="min-h-screen overflow-hidden  text-white flex justify-center items-center  bg-[url('/assets/images/bg02.jpg')] bg-cover bg-center bg-no-repeat  ">
       <div className="relative w-[350px] rounded-xl ">
         <div className="w-48 h-48 shape-1 rounded-full absolute -top-24 -left-24 "></div>
         <div className="w-48 h-48 shape-2 rounded-full absolute -bottom-24 -right-24 "></div>
